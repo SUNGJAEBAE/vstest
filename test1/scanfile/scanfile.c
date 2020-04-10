66d55ede5fa19b9ad2278e73b8ae864224f23e85
sungjae <hsm0156@gmail.com>
2020-04-10 17:26:28 +0900

    filescan

---
+++ test1/scanfile/scanfile.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int c;
+	FILE* file = NULL;
+	char file_name[] = "test.txt";
+	file = fopen(file_name, "r");
+	if (file == NULL)
+	{
+		printf("fail\n");
+		exit(1);
+	}
+	while ((c = getc(file)) != EOF) 
+	{
+		putchar(c);
+	}
+	fclose(file);
+	return 0;
+}
