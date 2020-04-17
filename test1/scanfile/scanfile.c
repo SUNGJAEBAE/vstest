3656797bd411ab0a7478b1c2497c14f2697c2cf4
sungjae <hsm0156@gmail.com>
2020-04-17 17:21:31 +0900

    read

--- test1/scanfile/scanfile.c
+++ test1/scanfile/scanfile.c
@@ -1,11 +1,30 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include "self.h"
-int x = 5;
-const static int main()
+#include <stdlib.h>
+int main(int argc, char* argv[])
 {
-	self();
-	self();
-	self();
+	int c;
+	FILE* file = NULL;
+	FILE* file2 = NULL;
+
+	if (argc != 2)
+	{
+		printf("wrong usage"); exit(EXIT_FAILURE);
+	}
+		
+	file = fopen(argv[1], "r");
+	file2 = fopen("test2.txt", "w");
+	if (file == NULL)
+	{
+		printf("wrong usage2");  exit(1);
+	}
+	while ((c = fgetc(file)) != EOF)
+	{
+		fputc(c, stdout);
+		fputc(c, file2);
+	}
+	fclose(file);
+
 	return 0;
 }
+
