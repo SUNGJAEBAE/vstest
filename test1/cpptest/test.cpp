f5402be56ac8cb3538af23e647194f686d924d36
sungjae <hsm0156@gmail.com>
2020-05-02 06:41:20 +0900

    ,,

--- test1/cpptest/test.cpp
+++ test1/cpptest/test.cpp
@@ -1,16 +1,23 @@
 #include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <limits>
+using namespace std;
 
-namespace qq::qq::qq	 {
-	void pr()
-	{
-		std::cout << "qq";
-	}
-}
 
 int main()
 {
-	int x = 5;
-	std::cout << "hello world!"<< x << std::endl;
-	qq::qq::qq::pr();
+	int row = 3;
+	int col = 5;
+	int** matrix = new int* [row];
+	for (int r = 0; r < row; r++)
+		matrix[r] = new int[col];
+	for (int r = 0; r < row; r++)
+		for (int c = 0; c < col; c++)
+			matrix[r][c] = r * 5 + c;
+	for (int r = 0; r < row; r++)
+		for (int c = 0; c < col; c++)
+			cout << matrix[r][c] << endl;
+
 	return 0;
 }
--- test1/scanfile/scanfile.c
+++ test1/scanfile/scanfile.c
@@ -1,22 +1,37 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <time.h>
+#include <stdlib.h>
 
-void delay(unsigned int sec)     // 특정 시간(초)만큼 기다리는 함수
+int main(int argc, char* argv[])
 {
-    clock_t ticks1 = clock();
-    clock_t ticks2 = ticks1;
-    while ((ticks2 / CLOCKS_PER_SEC - ticks1 / CLOCKS_PER_SEC) < (clock_t)sec)
-        ticks2 = clock();
-}
-
-int main()
-{
-    setvbuf(stdout, NULL, _IOFBF, 10);    // 출력 버퍼의 크기를 10으로 설정
-
-    printf("Hello, world!\n");
-
-    delay(3);    // 3초간 기다림
-
-    return 0;
+	int ch;
+	FILE* fr, *fw;
+	unsigned long count = 0;
+	const char* out_filename = "copy.txt";
+	char words[30] = "";
+	if (argc != 2)
+	{
+		printf("인수가 두 개여야 합니다");
+		exit(EXIT_FAILURE);
+	}
+	if ((fr = fopen(argv[1], "r")) == NULL)
+	{
+		printf("can't open %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	if ((fw = fopen(out_filename, "w")) == NULL)
+	{
+		printf("can't write %s\n", out_filename);
+		exit(EXIT_FAILURE);
+	}
+	while ((fscanf(stdin, "%30s", words)) != EOF)
+	{
+		fprintf(stdout, "%s\n", words);
+		fprintf(out_filename, "%s\n", words);
+		count++;
+	}
+	fclose(fr);
+	fclose(fw);
+	printf("\nfile %s has %lu characters\n", argv[1], count);
+	return 0;
 }
